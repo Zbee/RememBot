c8fed7473d6890cc6470f1dae07d01aa7d651ee6
Ethan Henderson <ethan@zbee.me>
2015-08-25 22:14:14 -0600

    Can see your created lists

--- manage/index.php
+++ manage/index.php
@@ -1,18 +1,26 @@
 <?php
 require("../header.php");
 
+$lOL = $lists->query()->where("id", ">", 0)->execute();
+
 if (array_key_exists("loggedin", $_GET))
   $e = sprintf($err, "You are logged in.");
 ?>
 
 <div id='body'>
   <div class='form'>
-    <h1>Manage your lists</h1>
+    <h1>Manage your lists, <?=$session["username"]?></h1>
     <?=$e?>
     <br>
-    <div class='alert'>You have <b>0</b> lists.</div>
-    <br>
     <a href='create' class='btn full'>Create a list</a>
+    <br>
+    <div class='alert'>You have <b><?=count($lOL)?></b> lists already.</div>
+    <?php
+    foreach ($lOL as $list) {
+      echo "<br>";
+      echo "<a href='edit?$list->id' class='btn full'>Edit $list->name</a>";
+    }
+    ?>
     <br>
     <a href='../help' class='btn half'>Help</a>
     <a href='../logout' class='btn half'>Log out</a>
