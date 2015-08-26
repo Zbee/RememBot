0d2583ae2624a08eadea9baff37912b06a3ac5dd
Ethan Henderson <ethan@zbee.me>
2015-08-26 16:52:38 -0600

    Made logging out delete the blobs

--- logout/index.php
+++ logout/index.php
@@ -5,21 +5,18 @@ $url = isset($_GET["url"]) ? $_GET["url"] : "../";
 $url .= "?loggedout";
 
 if (isset($_GET["specific"])) {
-  $logout = $USLite->logOut(
-    $_GET["specific"], $session["id"], false
-  );
-  if ($logout === true) $USLite->redirect301($url);
+  $logout = $USLite->logOut($_GET["specific"], $session["id"]);
 } elseif (isset($_GET["all"])) {
   $logout = $USLite->logOut(
-    $_COOKIE[SITENAME],
+    $_COOKIE[strtolower(SITE_NAME)],
     $session["id"],
     true,
     true
   );
-  if ($logout === true) $USLite->redirect301($url);
 } else {
   $logout = $USLite->logOut(
-    $_COOKIE[SITENAME], $session["id"], true
+    $_COOKIE[strtolower(SITE_NAME)], $session["id"], true
   );
-  $USLite->redirect301($url);
 }
+
+$USLite->redirect301($url);
